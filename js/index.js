b00fbef7a53235bb48324af2c2c112e9065ee000
umedjon01 <najottalimfn15@gmail.com>
2024-01-14 04:39:04 +0500

    UYGA VAZIFA

---
+++ js/index.js
@@ -0,0 +1,97 @@
+// 1chi masala
+
+//let num = 3;
+
+// if (num > 0) {
+//   console.log("musbat");
+// } else {
+//   console.log("manfiy");
+// }
+
+// 2chi masala
+
+// let son = 70;
+
+// if (son >= 90 && son <= 100) {
+//   console.log(`A`);
+// } else if (son >= 80 && son <= 89) {
+//   console.log(`B`);
+// } else if (son >= 70 && son <= 79) {
+//   console.log(`C`);
+// } else if (son >= 60 && son <= 69) {
+//   console.log(`D`);
+// } else if (son >= 0 && son <= 59) {
+//   console.log(`F`);
+// } else {
+//   console.log(`H`);
+// }
+
+// 3-chi masala
+
+// let year = 300;
+
+// if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+//   console.log("Kabisa yili ");
+// } else {
+//   console.log("Kabisa yili emas ");
+// }
+
+//  4chi masala
+
+// let son = -70;
+
+// if (son > 0) {
+//   console.log("Manfiy son");
+// } else if (son == 0) {
+//   console.log("nolga teng");
+// } else if (son < 0) {
+//   console.log("musbat son");
+// }
+
+//   5-chi masala
+
+// let num = 76;
+// let son = 7;
+
+// if (num > son) {
+//   console.log(num);
+// } else if (num < son) {
+//   console.log(son);
+// } else if (num == son) {
+//   console.log("ikkala sonteng");
+// }
+
+// 6-chi masala
+
+// let A = 5;
+// let B = 2;
+// let C = 3;
+
+// if (A < B && A < C) {
+//   console.log(A);
+// } else if (B < A && B < C) {
+//   console.log(B);
+// } else if (C < A && C < B) {
+//   console.log(C);
+// }
+
+//   7-chi masala
+
+// let a = 4;
+// let b = -7;
+// let c = 5;
+
+// let sum = 0;
+
+// if (a > 0) {
+//   sum = sum + a;
+// }
+// if (b > 0) {
+//   sum = sum + b;
+// }
+
+// if (c > 0) {
+//   sum = sum + c;
+// }
+
+// console.log(sum);
